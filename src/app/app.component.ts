8b66a03d7aea4a2a9a549ec390af798f76f8b299
timotuulio <timotuulio@hotmail.com>
2024-12-23 12:29:20 +0200

    Add picture as the header Change angular.json file so that src/assets is now my asset-folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,   // Declare AppComponent
-    SidebarComponent // Declare SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    // Declare SidebarComponent
   ],
   imports: [
     BrowserModule   // Import BrowserModule for browser compatibility
