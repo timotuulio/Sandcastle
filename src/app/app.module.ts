386641b0d270b58f65ed776bd4bd211c25e50ee7
timotuulio <timotuulio@hotmail.com>
2024-12-23 15:15:01 +0200

    Finish routing, add links to the sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+// Components
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from "./header/header.component";
 import { ConanPageComponent } from './conan-page/conan-page.component';
 import { HeManPageComponent } from './he-man-page/he-man-page.component';
 import { FranzPageComponent } from './franz-page/franz-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { FranzPageComponent } from './franz-page/franz-page.component';
     HeManPageComponent,
     FranzPageComponent,
     ConanPageComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,   // Import BrowserModule for browser compatibility
